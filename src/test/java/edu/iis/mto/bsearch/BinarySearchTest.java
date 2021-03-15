01b2bdc17e2745469f3c98d3bbc921add951fd2f
bartek839 <patoraban3@wp.pl>
2021-03-15 18:59:25 +0100

    testy_v1

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,11 +9,53 @@ import org.junit.jupiter.api.Test;
 class BinarySearchTest {
 
     @BeforeEach
-    void setUp() throws Exception {}
+    void setUp() throws Exception {
+    }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void oneElementSeqFoundTest() {
+        int key = 10;
+        int seq [] = {10};
+        SearchResult search = BinarySearch.search(key, seq);
+        assertTrue(search.isFound());
+        assertEquals(search.getPosition(), seq);
+    }
+
+    @Test
+    void oneElementSeqNotFoundTest(){
+        int key = 100;
+        int seq [] = {10};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertFalse(searchResult.isFound());
+        assertEquals(searchResult.getPosition(), -1);
+
+    }
+
+    @Test
+    void firstElementTest(){
+        int key = 10;
+        int seq [] = {10, 12, 89, 124, 400};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertTrue(searchResult.isFound());
+        assertEquals(searchResult.getPosition(), 1);
+    }
+
+    @Test
+    void lastElementTest(){
+        int key = 10;
+        int seq [] = {-40, 3, 5, 8, 10};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertTrue(searchResult.isFound());
+        assertEquals(searchResult.getPosition(), 5);
+    }
+
+    @Test
+    void centerElementTest(){
+        int key = 10;
+        int seq [] = {-4, 0, 1, 4, 5, 10, 14, 214, 500};
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertTrue(searchResult.isFound());
+        assertEquals(searchResult.getPosition(), 6);
     }
 
 }
